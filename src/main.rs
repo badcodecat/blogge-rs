87255438cad40d65e0116f483a8a34502f9a37bf
badcodecat <28487280+badcodecat@users.noreply.github.com>
2024-07-11 17:37:53 -0500

    Refactor to explicitly import macros

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
-#[macro_use] extern crate rocket;
-
-#[get("/")]
+#[rocket::get("/")]
 fn index() -> &'static str
 {
 	"Hello, World!"
 }
 
-#[launch]
+#[rocket::launch]
 fn rocket() -> _
 {
+	use rocket::routes;
 	rocket::build()
 		.mount("/", routes![index])
 }
